3cb15e3eb98e92d1f6badd2cecb44d66612aaf38
asher <aw0534145493@gmail.com>
2025-09-07 15:04:49 +0300

    Adding a class to get metadata from a file path

---
+++ app/metadata.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from datetime import datetime
+import json
+
+class Metadata:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def get_metadata(self):
+        metadata_list = []
+        path = Path(self.file_path)
+        stat  = path.stat()
+        metadata_list.append({"name": path.name})
+        metadata_list.append({"file_extension": path.suffix})
+        metadata_list.append({"size": stat.st_size})
+        metadata_list.append({"creation_date": stat.st_ctime})
+        metadata = json.dumps(metadata_list)
+        return metadata
+
+
+metadat = Metadata( r"C:\Users\user1\Desktop\podcasts\download (1).wav")
+print(metadat.get_metadata())
